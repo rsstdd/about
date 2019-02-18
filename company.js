ec0e183db8e0efd950023e254476660628ffe736
Ross Todd <rosstod@gmail.com>
2019-02-18 09:56:48 -0800

    feature(example-3): Add company w/Migoites

---
+++ company.js
@@ -0,0 +1,110 @@
+export default [
+  {
+    imgClass: "cards-image-3--5",
+    liUrl: "https://www.linkedin.com/in/jeff-warren-b550023/",
+    firstName: "Jeff",
+    role: "CEO"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/vince-cheng-584697/",
+    firstName: "Vince",
+    role: "Product"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/shanmugarasa-muhunthan-9b306159/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_people%3BZ%2FO1XEtYQS2PkLrXJmPSOw%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_search_srp_people-search_srp_result&lici=G2SDtbTfTJm5%2BPP2sPAx1g%3D%3D",
+    firstName: "Shan",
+    role: "CTO & Head of Engineering"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/kathy-van-buskirk-8850895/",
+    firstName: "Kathy",
+    role: "PR & Marketing"
+  },
+  {
+    imgClass: "cards-image-3--2",
+    liUrl: "https://www.linkedin.com/in/rachelle-rathbone/",
+    firstName: "Rachelle",
+    role: "Engineer"
+  },
+  {
+    imgClass: "cards-image-3--3",
+    liUrl: "https://www.linkedin.com/in/rsstdd/",
+    firstName: "Ross",
+    role: "Engineer"
+  },
+  {
+    imgClass: "cards-image-3--4",
+    liUrl: "https://www.linkedin.com/in/lee-hasiuk-08a418/",
+    firstName: "Lee",
+    role: "Engineer"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/elizabethkolakowski/",
+    firstName: "Beth",
+    role: "Marketing"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/courtney-j-schneider/",
+    firstName: "Courtney",
+    role: "Marketing"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/michael-schober-2a243a16/",
+    firstName: "Mike",
+    role: "Engineer"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/tarikm2/",
+    firstName: "Tarik",
+    role: "Engineer"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/dylanharper-data/",
+    firstName: "Dylan",
+    role: "Analytics"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/yamamotorei/",
+    firstName: "Rei",
+    role: "UX Researcher"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/natalie-riendeau-31699598/",
+    firstName: "Natalie",
+    role: "Office Manager"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/joe-rogers-b15823/",
+    firstName: "Joe",
+    role: "Engineer"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/joe-rogers-b15823/",
+    firstName: "Matt",
+    role: "User Acquisition Manager"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/jeremy-xavier-203b66b/",
+    firstName: "Jeremy",
+    role: "Marketing"
+  },
+  {
+    imgClass: "cards-image-3--1",
+    liUrl: "https://www.linkedin.com/in/stefany-sterling-711860141/",
+    firstName: "Stefany",
+    role: "Content Creator"
+  },
+]
